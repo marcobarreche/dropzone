2d899466b31263475c2ca8a51ce5537a269d6d95
Marcos Antonio Barreche Salguero <marcobarreche@gmail.com>
2013-09-16 11:39:58 +0000

    Create policy and signature in the server.

--- uploader/static/js/s3.uploader.js
+++ uploader/static/js/s3.uploader.js
@@ -1,83 +1,58 @@
-
 $(function() {
-    // Constants
-    var _AWS_ACCESS_KEY = 'acess_key';
-    var _AWS_SECRET_KEY = 'secret_key';
 
-    var BUCKET_NAME = 'barreche-tb';
-    var BUCKET_RESOURCE_FOLDER = 'test/';
-    var BUCKET_ACTION = 'https://' + BUCKET_NAME + '.s3.amazonaws.com';
-    var BUCKET_URL = "https://s3.amazonaws.com/" + BUCKET_NAME + '/';
-    var THUMBRIT = {
-        'BASE_URL': 'http://api.thumbr.io/',
-        'API_KEY': 'api_key',
-        'SECRET_KEY': 'secret_key',
-        'WIDTH_THUMBNAIL': 100,
-        'HEIGHT_THUMBNAIL': 100,
-    };
-    var CLASS_DETAILS_FILE = 'dz-details';
+var CREATE_POLICY_SERVICE = 'http://thumbr.io/get-policy-and-signature',
+    GET_THUMBRIO_URL = 'http://thumbr.io/get-thumbrio',
+    BUCKET_NAME = 'barreche-tb',
+    BUCKET_RESOURCE_FOLDER = 'test/',
+    API_KEY = '...',
+    AWS_ACCESS_KEY = '...';
+var BUCKET_ACTION = 'https://' + BUCKET_NAME + '.s3.amazonaws.com',
+    BUCKET_URL = "https://s3.amazonaws.com/" + BUCKET_NAME + '/';
+var CLASS_DETAILS_FILE = 'dz-details';
 
-    
-    var myDropzone = new Dropzone('#uploader', {url: BUCKET_ACTION});
-    myDropzone.on("sending", function(file, xhr, formData) {
-        uploadFile(file, xhr, formData, _AWS_ACCESS_KEY, _AWS_SECRET_KEY);
+var myDropzone = new Dropzone('#uploader', {url: BUCKET_ACTION});
+myDropzone.on("sending", function(file, xhr, formData) {
+    uploadFile(file, xhr, formData, AWS_ACCESS_KEY);
+});
+
+myDropzone.on("success", function(file, response) {
+    updateUrl(file.name, $(file.previewElement));
+});
+
+function uploadFile(file, xhr, formData, access_key) {
+    $.ajax({
+        url: CREATE_POLICY_SERVICE,
+        async: false,
+        dataType: 'json',
+        success: function(policyAndSignature) {
+            var policy = policyAndSignature[0];
+            var signature = policyAndSignature[1];
+            var key = BUCKET_RESOURCE_FOLDER + file.name;
+            formData.append('key', key);
+            formData.append('acl', 'public-read');
+            formData.append('Content-Type', file.type);
+            formData.append('AWSAccessKeyId', access_key);
+            formData.append('policy', policy);
+            formData.append('signature', signature);
+            formData.append('file', file);
+            xhr.open('POST', BUCKET_ACTION, true);
+            xhr.send(formData);
+        }
     });
-    myDropzone.on("success", function(file, response) {
-        updateUrl(file.name, $(file.previewElement));
+}
+
+function updateUrl(filename, element) {
+    var queryArguments = '?url=' + BUCKET_URL + BUCKET_RESOURCE_FOLDER + filename;
+    queryArguments += '&size=' + THUMBRIT.WIDTH_THUMBNAIL + 'x' + THUMBRIT.HEIGHT_THUMBNAIL + 'c';
+
+    $.ajax({
+        url: GET_THUMBRIO_URL + queryArguments,
+        async: false,
+        dataType: 'json',
+        success: function(thumbrioUrl) {
+            element.find('.' + CLASS_DETAILS_FILE + ' img').attr('src', thumbrioUrl);
+        }
     });
+}
 
-    function uploadFile(file, xhr, formData, access_key, secret_key) {
-        var policyAndSignature = createPoliceText(secret_key);
-        var policy = policyAndSignature[0];
-        var signature = policyAndSignature[1];
-        var key = BUCKET_RESOURCE_FOLDER + file.name;
-
-        formData.append('key', key);
-        formData.append('acl', 'public-read');
-        formData.append('Content-Type', file.type);
-        formData.append('AWSAccessKeyId', access_key);
-        formData.append('policy', policy);
-        formData.append('signature', signature);
-        formData.append('file', file);
-
-        xhr.open('POST', BUCKET_ACTION, true);
-        xhr.send(formData);
-    }
-
-    function createPoliceText(secret) {
-        var POLICY_JSON = {
-            "expiration": "2015-01-01T00:00:00Z",
-            "conditions": [
-                {"bucket": BUCKET_NAME},
-                ["starts-with", "$key", BUCKET_RESOURCE_FOLDER],
-                {"acl": "public-read"},
-                ["starts-with", "$Content-Type", ""],
-                ["content-length-range", 0, 1048576]
-            ]
-        };
-        var policyBase64 = Base64.encode(JSON.stringify(POLICY_JSON));
-        var signature = b64_hmac_sha1(secret, policyBase64) + '=';
-        return [policyBase64, signature];
-    }
-
-    function updateUrl(filename, element) {
-        var url = BUCKET_URL + BUCKET_RESOURCE_FOLDER + filename;
-        var thumbritUrl = thumbrit(url, THUMBRIT.WIDTH_THUMBNAIL, THUMBRIT.HEIGHT_THUMBNAIL,
-                                   '', false, [], filename);
-        element.find('.' + CLASS_DETAILS_FILE + ' img').attr('src', thumbritUrl);
-    }
-
-    function thumbrit(url, width, height, crop, download, effects, seoname) {
-        var encodedUrl = encodeURIComponent(url.replace('http://', '')).replace(/%2F/g, '/'),
-            sizeAndOptions = width + 'x' + height + crop,
-            i;
-        if (download)
-            sizeAndOptions += '-d';
-        for (i = 0; i < effects.length; i++)
-            if (effects[i])
-                sizeAndOptions += '-e' + effects[i];
-        var urlToSecure = THUMBRIT.BASE_URL + THUMBRIT.API_KEY + '/' + encodedUrl + '/' + sizeAndOptions + '/' + seoname;
-        var hmac = Crypto.HMAC(Crypto.MD5, urlToSecure, THUMBRIT.SECRET_KEY);
-        return THUMBRIT.BASE_URL + hmac + '/' + THUMBRIT.API_KEY + '/' + encodedUrl + '/' + sizeAndOptions + '/' + seoname;
-    }
 });
--- uploader/uploader.py
+++ uploader/uploader.py
@@ -1,14 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import sys
+import datetime
+import json
+import base64
+import hashlib
+import hmac
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+AWS_SECRET_KEY = '...'
+BUCKET_NAME = 'barreche-tb';
+BUCKET_RESOURCE_FOLDER = 'test/';
 
 
 @app.route('/')
 def init():
     return render_template('uploader.html')
+
+
+@app.route('/get-policy-and-signature')
+def get_policy_and_signature():
+    """
+    It returns the policy and the signature to s3.
+    Returns:
+        (str): It returns the list [policy, signature] in a json string. 
+    """
+    # The secret key is AWS_SECRET_KEY by default.
+    secret_key = AWS_SECRET_KEY
+
+    # The expiration date is one year later.
+    expiration_date = datetime.datetime.now() + datetime.timedelta(days=365)
+    policy_document = {
+        "expiration": expiration_date.strftime('%Y-%m-%dT00:00:00Z'),
+        "conditions": [
+            {"bucket": BUCKET_NAME},
+            ["starts-with", "$key", BUCKET_RESOURCE_FOLDER],
+            {"acl": "public-read"},
+            ["starts-with", "$Content-Type", ""],
+            ["content-length-range", 0, 1048576]
+        ]
+    }
+    policy = base64.b64encode(json.dumps(policy_document))
+    signature = base64.b64encode(hmac.new(secret_key, policy, hashlib.sha1).digest())
+    return Response(json.dumps([policy, signature]), status=200)
 
 
 if __name__ == '__main__':
