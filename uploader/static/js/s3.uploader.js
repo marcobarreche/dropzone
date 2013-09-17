162306abf83f46567aa0ed6ded6157154cc02c2b
Marcos Antonio Barreche Salguero <marcobarreche@gmail.com>
2013-09-17 16:56:20 +0000

    Update html

--- uploader/static/js/s3.uploader.js
+++ uploader/static/js/s3.uploader.js
@@ -1,13 +1,16 @@
 $(function() {
 
-var CREATE_POLICY_SERVICE = 'http://thumbr.io/get-policy-and-signature',
-    GET_THUMBRIO_URL = 'http://thumbr.io/get-thumbrio',
+var API_KEY = '...',
+    AWS_ACCESS_KEY = '...',
+    THUMBRIT_SERVER = '....',
     BUCKET_NAME = 'barreche-tb',
-    BUCKET_RESOURCE_FOLDER = 'test/',
-    API_KEY = '...',
-    AWS_ACCESS_KEY = '...';
-var BUCKET_ACTION = 'https://' + BUCKET_NAME + '.s3.amazonaws.com',
+    BUCKET_RESOURCE_FOLDER = 'uploader/';
+
+var CREATE_POLICY_AND_SIGNATURE_SERVICE = THUMBRIT_SERVER + '/get/policy_and_signature?api_key=' + API_KEY,
+    GET_THUMBRIO_URL = THUMBRIT_SERVER + '/get/thumbrio_api_url?api_key=' + API_KEY,
+    BUCKET_ACTION = 'https://' + BUCKET_NAME + '.s3.amazonaws.com',
     BUCKET_URL = "https://s3.amazonaws.com/" + BUCKET_NAME + '/';
+
 var CLASS_DETAILS_FILE = 'dz-details';
 
 var myDropzone = new Dropzone('#uploader', {url: BUCKET_ACTION});
@@ -16,12 +19,12 @@ myDropzone.on("sending", function(file, xhr, formData) {
 });
 
 myDropzone.on("success", function(file, response) {
-    updateUrl(file.name, $(file.previewElement));
+    updateUrl(file.name, $(file.previewElement), API_KEY);
 });
 
 function uploadFile(file, xhr, formData, access_key) {
     $.ajax({
-        url: CREATE_POLICY_SERVICE,
+        url: CREATE_POLICY_AND_SIGNATURE_SERVICE,
         async: false,
         dataType: 'json',
         success: function(policyAndSignature) {
@@ -43,7 +46,8 @@ function uploadFile(file, xhr, formData, access_key) {
 
 function updateUrl(filename, element) {
     var queryArguments = '?url=' + BUCKET_URL + BUCKET_RESOURCE_FOLDER + filename;
-    queryArguments += '&size=' + THUMBRIT.WIDTH_THUMBNAIL + 'x' + THUMBRIT.HEIGHT_THUMBNAIL + 'c';
+    queryArguments += '&width=' + THUMBRIT.WIDTH_THUMBNAIL;
+    queryArguments += '&height=' + THUMBRIT.HEIGHT_THUMBNAIL;
 
     $.ajax({
         url: GET_THUMBRIO_URL + queryArguments,
