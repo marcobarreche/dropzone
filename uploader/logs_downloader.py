009d7820abcfb162f9099f132b13fa168bcc73e8
Marcos Antonio Barreche Salguero <marcobarreche@gmail.com>
2013-09-27 15:21:20 +0000

    Fix some errors in logs_downloader

--- uploader/logs_downloader.py
+++ uploader/logs_downloader.py
@@ -5,13 +5,11 @@ import hmac
 from boto.s3.connection import S3Connection
 
 
-SERVICE_OPTIONS = {
-    's3': {
-        'access_key': 'AKIAIM7RS3YPLGU2I6YQ',
-        'secret_key': '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',
-        'bucket_name': 'barreche-tb',
-        'path': '/logs/'
-    }
+LOGGER_SERVICE_S3 = {
+    'access_key': 'AKIAIM7RS3YPLGU2I6YQ',
+    'secret_key': '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',
+    'bucket_name': 'barreche-tb',
+    'path': '/logs/'
 }
 
 
@@ -22,21 +20,20 @@ def calculate_authorization(string_to_sign, access_key, secret_key):
     )
 
 
-def create_request_list_objects(s3_folder, local_folder, date=None, debug=False):
+def create_request_list_objects(local_folder, date=None, debug=False):
     """
     Download all the files inside the folder s3_folder and download all files into local_folder.
 
     Arguments:
-        s3_folder (str): The folder in s3. It must start and end in '/'.
         local_folder (str): The local folder where the files will be saved. It must end in '/'.
         date (datetime.datetime): Optional. If you specify a date, only the all logs after this
                                             date will be download.
         debug (boolean): Optional. True if you want to see what happens inside the function.
                                    False, otherwise
     """
-    conn = S3Connection(SERVICE_OPTIONS.get('s3').get('access_key'), SERVICE_OPTIONS.get('s3').get('secret_key'))
-    mybucket = conn.get_bucket(SERVICE_OPTIONS.get('s3').get('bucket_name'), validate=False)
-    path = s3_folder[1:]
+    conn = S3Connection(LOGGER_SERVICE_S3.get('access_key'), LOGGER_SERVICE_S3.get('secret_key'))
+    mybucket = conn.get_bucket(LOGGER_SERVICE_S3.get('bucket_name'), validate=False)
+    path = LOGGER_SERVICE_S3.get('path')[1:]
     for key in mybucket.list():
         if not (key.name).startswith(path):
             continue
@@ -49,10 +46,19 @@ def create_request_list_objects(s3_folder, local_folder, date=None, debug=False)
         key.get_contents_to_filename(filename)
 
 
-def eliminate_list_objects_before_date(s3_folder, date=None, debug=False):
-    conn = S3Connection(SERVICE_OPTIONS.get('s3').get('access_key'), SERVICE_OPTIONS.get('s3').get('secret_key'))
-    mybucket = conn.get_bucket(SERVICE_OPTIONS.get('s3').get('bucket_name'), validate=False)
-    path = s3_folder[1:]
+def eliminate_list_objects_before_date(date=None, debug=False):
+    """
+    Delete all logs older than a specific date.
+
+    Arguments:
+        date (datetime.datetime): Optional. If you don't specify a date, all logs will be deleted.
+        debug (boolean): Optional. True if you want to see what happens inside the function.
+                                   False, otherwise
+    """
+    conn = S3Connection(LOGGER_SERVICE_S3.get('access_key'),
+                        LOGGER_SERVICE_S3.get('secret_key'))
+    mybucket = conn.get_bucket(LOGGER_SERVICE_S3.get('bucket_name'), validate=False)
+    path = LOGGER_SERVICE_S3.get('path')[1:]
     for key in mybucket.list():
         if not (key.name).startswith(path):
             continue
